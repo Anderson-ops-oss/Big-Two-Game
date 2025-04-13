40fa8995dd4369afc660ebbb89e790f17c786915
A zzz <136796251+Anderson-ops-oss@users.noreply.github.com>
2025-04-14 01:35:40 +0800

    version_1

--- GameMessage.java
+++ GameMessage.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 
 /**
  * This class is used to model the message for a network game.
- * @author Kenneth Wong
+ * 
  *
  */
 public class GameMessage implements Serializable {
